f35c366f766e83f0fe9282dd3f4bcafad4ccd735
Kalejaiye Tolulope <tolulopekay56@gmail.com>
2022-10-01 13:30:11 -0700

    omo

--- src/App.js
+++ src/App.js
@@ -1,32 +1,30 @@
-import React, {useState} from "react";
+import React, {createContext} from "react";
+
+
+//create context for the app
+const authContext = createContext()
+
+
+function UserProfile (){
+  return(
+    <div>
+      <h1>User Profile</h1>
+      <div>Username: tolulopekay</div>
+      <div>Name:Kalejaiye Tolulope</div>
+      <div>Location: Ikeja,Lagos</div>
+    </div>
+  )
+
+}
 
 
 
-
-
-// Create Our root component => App component
- function App(){
-  //create a state variable
-  const[count, setCount]  = useState(0);
-
-  //create a function to increment the count
-  const increment = (event) => {
-    event.prevDefault();
-    setCount(prevCount => prevCount + 1);
-  }
-
-  //create a function to decrement the count
-  const decrement = (event) => {
-    event.prevDefault();
-    setCount (prevCount => prevCount - 1)
-  }
+const App = () => {
 
 
 
  return  <section className="main-container">
-    <h1>Count:{count}</h1>
-    <button onClick={increment}>Increment</button>
-    <button onClick={decrement}>Decrement</button>
+   <UserProfile/>
   </section>
 
  }
